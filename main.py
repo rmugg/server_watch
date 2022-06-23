88cf631e21b854d13dadae7d1ede09aa9cedefaf
rmugg <64331791+rmugg@users.noreply.github.com>
2022-06-23 12:46:22 -0400

    Update main.py
    Changed GDK to GTK

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import signal
 import string
 import gi
-gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 from gi.repository import AppIndicator3
